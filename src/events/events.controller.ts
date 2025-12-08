6d7cd692986b5e6a183bca40455b6f4334f78fb7
Tanish <tanishdu13@gmail.com>
2025-12-08 18:25:44 +0530

    added sync for events

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -5,18 +5,24 @@ import {
   HttpCode,
   HttpStatus,
   Param,
+  Post,
+  Req,
   UseGuards
 } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { JwtAuthGuard } from 'src/auth/guards';
 import { CreateEventDTO } from './dto/create-event.dto';
 import { plainToInstance } from 'class-transformer';
+import { GmailService } from './providers/gmail/gmail.service';
 
 @Controller('events')
 export class EventsController {
-  constructor(private readonly eventService: EventsService) {}
+  constructor(
+    private readonly eventService: EventsService,
+    private readonly gmailService: GmailService
+  ) {}
 
-  @Get('all')
+  @Get()
   @UseGuards(JwtAuthGuard)
   @HttpCode(HttpStatus.OK)
   async getAllEvents(): Promise<CreateEventDTO[]> {
@@ -29,7 +35,7 @@ export class EventsController {
   @Get(':id')
   @UseGuards(JwtAuthGuard)
   @HttpCode(HttpStatus.OK)
-  async getEventById(@Param('id') id: number): Promise<CreateEventDTO> {
+  async getEventById(@Param('id') id: string): Promise<CreateEventDTO> {
     const event = await this.eventService.getEventById(id);
     return plainToInstance(CreateEventDTO, event, {
       excludeExtraneousValues: true
@@ -51,7 +57,17 @@ export class EventsController {
   @Delete(':id')
   @UseGuards(JwtAuthGuard)
   @HttpCode(HttpStatus.NO_CONTENT)
-  async deleteEvent(@Param('id') id: number): Promise<void> {
+  async deleteEvent(@Param('id') id: string): Promise<void> {
     await this.eventService.deleteEvent(id);
+  }
+
+  @Post('sync')
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(HttpStatus.ACCEPTED)
+  async syncEvents(@Req() req): Promise<void> {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const userId = req.user?.id;
+    await this.gmailService.fetchNotifications(userId);
+    //later we can add other providers here
   }
 }
